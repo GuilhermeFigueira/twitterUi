5adff1c130294aaed97f498c007e37877e9b1bcb
Guilherme Figueira <guibf2004@gmail.com>
2023-02-13 17:49:09 -0300

    sidebar melhor (inacabada)

--- src/components/Sidebar/SidebarMenu.tsx
+++ src/components/Sidebar/SidebarMenu.tsx
@@ -13,29 +13,64 @@ import SidebarItem from "./SidebarItem";
 
 export default function Sidebar() {
 	return (
-		<aside className="py-6 px-5 flex flex-col gap-8">
-			<TwitterLogo size={32} weight="fill" />
-			<nav className="flex flex-col gap-y-8">
-				<SidebarItem
-					className="text-twitter-blue"
-					icon={<House weight="fill" className="text-twitter-blue" />}
+		<aside className="flex flex-col justify-between h-screen">
+			<div className="py-2 px-3 flex flex-col ">
+				<div>
+					<TwitterLogo size={32} weight="fill" />
+				</div>
+				<nav className="flex flex-col gap-y-8">
+					<SidebarItem
+						className="text-twitter-blue"
+						icon={
+							<House
+								weight="fill"
+								className="text-twitter-blue"
+							/>
+						}
+					>
+						Home
+					</SidebarItem>
+					<SidebarItem icon={<Hash />}>Explore</SidebarItem>
+					<SidebarItem icon={<Bell />}>Notifications</SidebarItem>
+					<SidebarItem icon={<Envelope />}>Messages</SidebarItem>
+					<SidebarItem icon={<BookmarkSimple />}>
+						Bookmarks
+					</SidebarItem>
+					<SidebarItem icon={<FileText />}>Lists</SidebarItem>
+					<SidebarItem icon={<User />}>Profile</SidebarItem>
+					<SidebarItem icon={<DotsThreeCircle />}>More</SidebarItem>
+				</nav>
+				<button
+					type="button"
+					className="bg-twitter-blue rounded-full flex justify-center w-full text-white text-lg font-black p-3 hover:brightness-90"
 				>
-					Home
-				</SidebarItem>
-				<SidebarItem icon={<Hash />}>Explore</SidebarItem>
-				<SidebarItem icon={<Bell />}>Notifications</SidebarItem>
-				<SidebarItem icon={<Envelope />}>Messages</SidebarItem>
-				<SidebarItem icon={<BookmarkSimple />}>Bookmarks</SidebarItem>
-				<SidebarItem icon={<FileText />}>Lists</SidebarItem>
-				<SidebarItem icon={<User />}>Profile</SidebarItem>
-				<SidebarItem icon={<DotsThreeCircle />}>More</SidebarItem>
-			</nav>
-			<button
-				type="button"
-				className="bg-twitter-blue rounded-full flex justify-center w-full text-white text-lg font-black p-3 hover:brightness-90"
-			>
-				Tweet
-			</button>
+					Tweet
+				</button>
+			</div>
+			<div className="flex  items-center p-3">
+				<img
+					src="https://github.com/guilhermefigueira.png"
+					alt=""
+					className="w-10 h-10"
+				/>
+				<div className="flex flex-col">
+					<h1 className="text-transparent bg-clip-text bg-gradient-to-r from-rose-700 to-sky-700">
+						Made by{" "}
+						<a
+							href="https://github.com/guilhermefigueira.png"
+							target="_blank"
+							className="underline"
+						>
+							{" "}
+							Guilherme Figueira
+						</a>
+					</h1>
+					<h2>@GuilhermeFigueira</h2>
+				</div>
+				<div className="flex flex-1 justify-end">
+					<DotsThreeCircle className="float-right" />
+				</div>
+			</div>
 		</aside>
 	);
 }
--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -3,27 +3,31 @@ import { Sparkle } from "phosphor-react";
 export default function Timeline() {
 	return (
 		<div className="border-x-[1px] border-solid border-[#ebeef0]">
-			<div className="border-b-[1px] border-solid border-[#ebeef0] flex p-5 items-center justify-between">
+			<div className="border-b-[1px] border-solid border-[#ebeef0] flex py-6 px-5 items-center justify-between">
 				<h1 className="font-bold text-xl">Home</h1>
 				<Sparkle size={24} className="text-twitter-blue" />
 			</div>
-			<form className="">
-				<label htmlFor="">
+			<form className=" py-6 px-5 flex flex-col gap-2">
+				<label htmlFor="" className=" flex gap-2">
 					<img
-						className="rounded-full w-14"
+						className="rounded-full w-12 h-12"
 						src="https://github.com/guilhermefigueira.png	"
 						alt="Guilherme de Biazi Figueira"
 					/>
 					<textarea
-						className=""
+						className="flex-1 text-xl font-medium mt-3 resize-none focus:outline-none placeholder:text-[#5b7083]"
 						id="tweet"
 						placeholder="O que está acontecendo?"
 					/>
 				</label>
-				<button className="" type="submit">
+				<button
+					className=" bg-twitter-blue ml-auto rounded-full py-3 px-6 text-white text-base font-black hover:brightness-90"
+					type="submit"
+				>
 					Tweetar
 				</button>
 			</form>
+			<div className="bg-[#f7f9fa] border-t-[1px] border-solid h-3" />
 		</div>
 	);
 }
